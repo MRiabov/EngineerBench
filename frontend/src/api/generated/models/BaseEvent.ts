bb0421a12860938af453f7755b14488be60d4cff
MRiabov <maksymriabov2004@gmail.com>
2026-04-13 18:11:46 +0100

    (refactor) Remove frontend app bundle

--- frontend/src/api/generated/models/BaseEvent.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * Base class for all observability events.
- */
-export type BaseEvent = Record<string, any>;
--- frontend/src/api/generated/models/IntegrationTestStatusResponse.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type IntegrationTestStatusResponse = {
-    is_integration_test: boolean;
-};
-
